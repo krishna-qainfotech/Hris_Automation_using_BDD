c0bbf3e86f9f9251affc511e0f132887e153d8cd
krishna-qainfotech <40290540+krishna-qainfotech@users.noreply.github.com>
2018-08-09 12:38:21 +0530

    Update RunnerClass.java

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -1,15 +1,15 @@
-package Runner;
+//package Runner;
 
-import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+// import org.junit.runner.RunWith;
+// import cucumber.api.CucumberOptions;
+// import cucumber.api.junit.Cucumber;
  
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features ="/home/qainfotech/eclipse-workspace/HRIS_USING_BDD_FRAMEWORK/src/test/java/Feature/Hris.feature"
-		,glue={"StepDefination"}
-		)
+// @RunWith(Cucumber.class)
+// @CucumberOptions(
+// 		features ="/home/qainfotech/eclipse-workspace/HRIS_USING_BDD_FRAMEWORK/src/test/java/Feature/Hris.feature"
+// 		,glue={"StepDefination"}
+// 		)
 
-public class RunnerClass {
+// public class RunnerClass {
 
-}
+// }
